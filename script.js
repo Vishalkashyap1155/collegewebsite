598f45d851d504a95fa8d07768ab0a30a130ee23
vishalkashyap1155 <vishalkashyap1155@gmail.com>
2025-07-20 13:55:17 +0530

    add html css and js file

---
+++ script.js
@@ -0,0 +1,11 @@
+/*-------------- java script for nav toggle menu---------------------*/
+var navLinks = document.getElementById("navLinks")
+
+function showMenu(){
+    navLinks.style.right = "0";
+}
+
+
+function hideMenu(){
+    navLinks.style.right = "-200px";
+}
